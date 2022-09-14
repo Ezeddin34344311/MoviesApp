c33769126181619805495a248888581a167ba3f1
Ezeddin Ahmed <hadouezeddin@gmail.com>
2022-09-14 22:41:01 +0000

    create project Movies App

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/provid/my_provider.dart';
+import 'package:provider/provider.dart';
+import 'home/home_screen.dart';
+import 'modules/tabs_navBar/browser/Screen/movies_filtred.dart';
+//_____________________________________________________________________________
+main(){
+  runApp(ChangeNotifierProvider(
+      create: (context){
 
-void main() {
-  runApp( MyApp());
+        return MyProviderApp();
+      },
+      child: MyApp()));
 }
 
+
 class MyApp extends StatelessWidget {
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              selectedItemColor: Colors.yellow
+          )
+      ),
+      routes: {
+        HomePage.routename : (context) => HomePage(),
+        MoviesFiltred.routeName: (context) => MoviesFiltred()
+      },
+      initialRoute: HomePage.routename,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
